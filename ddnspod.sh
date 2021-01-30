d6d99041047925b5cff4566e34d5f3be5a7e3ea9
chuan228 <78264020+chuan228@users.noreply.github.com>
2021-01-30 21:44:32 +0800

    Update ddnspod.sh

--- ddnspod.sh
+++ ddnspod.sh
@@ -5,7 +5,7 @@
 . /your_real_path/ardnspod
 
 # Combine your token ID and token together as follows
-arToken="12345,7676f344eaeaea9074c123451234512d"
+arToken="211003，441b08f4ae6032ff710fbcb37c5e97a6"
 
 # Place each domain you want to check as follows
 # you can have multiple arDdnsCheck blocks
